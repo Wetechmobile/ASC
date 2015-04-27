c1217995d91f18aa20652f410851c74a82c4315a
Wetechmoiblie <imfo@wetechmobile.com>
2015-04-27 13:40:33 -0400

    Files

---
+++ assets/ritz-tnt-wabe/Ritz tnt wabe_edgeActions.js
@@ -0,0 +1,10 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",10922,function(sym,e){sym.play(0);});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-42768754");
